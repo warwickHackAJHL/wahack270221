2ee8e1959e2bec38d16a27785ecb1b03cfc1f627
hugo-wb <hugowong71@gmail.com>
2021-02-27 16:19:47 +0000

    image fix

--- src/Canvas.js
+++ src/Canvas.js
@@ -37,7 +37,7 @@ const Canvas = ({ setCanvas, count, setCount }) => {
         position="relative"
         bottom="-15px"
         left="40px"
-        colorScheme="yellow"
+        colorScheme="red"
         onClick={() => {
           if (canvasRef?.current) {
             canvasRef.current.undo();

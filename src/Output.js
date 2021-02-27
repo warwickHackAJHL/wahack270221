3b5bdb0d04d4e60faa3b00487aebcc1868b81dcf
Justin Tan How Yuan <justanhowyuan@gmail.com>
2021-02-27 14:34:58 +0000

    Tess should work

--- src/Output.js
+++ src/Output.js
@@ -1,19 +1,44 @@
 // file to contain the output text
 import React from 'react';
-import Tesseract from 'tesseract.js';
-import {useState} from 'react';
+import { createWorker } from 'tesseract.js';
+import { useState } from 'react';
 import { Flex,Text } from '@chakra-ui/react';
 
 function Output({imageInput,canvasInput}) {
   // switch it up depending on canvas/image
-  const [output, setOutput] = useState('fake output');
-  Tesseract.recognize(
-    'https://tesseract.projectnaptha.com/img/eng_bw.png',
-    'eng',
-    // { logger: m => console.log(m) }
-  ).then((data) => {
-    console.log(data)
+  const [output, setOutput] = useState();
+  const [loading, setLoading] = useState(true);
+
+  if (imageInput == undefined && canvasInput == undefined) {
+    return <div></div>;
+  }
+
+  scanInput(); // 
+  if (loading) {
+    return <div>Scanning...</div>;
+  }
+  
+
+  const worker = createWorker({
+    logger: m => console.log(m),
   });
+
+  const scanInput = async () => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng'); 
+    const { data: {text} } = await worker.recognize(imageInput ?? canvasInput, 'eng');
+    setOutput(text);
+    setLoading(false);
+    await worker.terminate(); 
+  }
+  
+  
+  // .then( data : {text} ) => { // Look at comment below for data properties
+  //   console.log(data)
+  //   setOutput(data.text);
+  // });
+
   return(
     <Flex>
       <Text>
@@ -22,5 +47,44 @@ function Output({imageInput,canvasInput}) {
     </Flex>
   )
 }
+
+// {
+//   text: "Mild Splendour of the various-vested Nig ..."
+//   hocr: "<div class='ocr_page' id= ..."
+//   tsv: "1 1 0 0 0 0 0 0 1486 ..."
+//   box: null
+//   unlv: null
+//   osd: null
+//   confidence: 90
+//   blocks: [{...}]
+//   psm: "SINGLE_BLOCK"
+//   oem: "DEFAULT"
+//   version: "4.0.0-825-g887c"
+//   paragraphs: [{...}]
+//   lines: (8) [{...}, ...]
+//   words: (58) [{...}, {...}, ...]
+//   symbols: (295) [{...}, {...}, ...]
+// }
+// If you want to see the complete result object, visit the URL https://omed.hplar.ch/webocr/basic.html and open the developer console.
+// detect.data holds information about detected script and orientation and the corresopnding confidence score
+// {
+//   tesseract_script_id: 1
+//   script: "Latin"
+//   script_confidence: 39.58333969116211
+//   orientation_degrees: 0
+//   orientation_confidence: 29.793731689453125
+// }
+
+  // const worker = Tesseract.createWorker({
+  //   logger: m => console.log(m)
+  // });
+
+  // Tesseract.setLogging(true); // needed to check OCR progress
+
+  // await worker.load();
+  // await worker.loadLanguage('eng');
+  // await worker.initialize('eng');
+
+
 
 export default Output;
